2ff06e75c37b79a432578cc43cde1852bc680e9f
yasinklausss1 <yasin.klaus@igs-peine.de>
2025-08-03 20:39:13 +0200

    Delete supabase directory

--- supabase/migrations/20250725185634-4927feee-1819-4298-b224-d6ff9d2416ba.sql
+++
@@ -1,6 +0,0 @@
--- Deactivate old testnet address for admin user ADMkz
-UPDATE bitcoin_addresses 
-SET is_active = false 
-WHERE user_id IN (
-  SELECT user_id FROM profiles WHERE username = 'ADMkz'
-) AND is_active = true;
--- supabase/migrations/20250726150051-23e18763-29b9-4f21-94c5-a87817948fe0.sql
+++
@@ -1,2 +0,0 @@
--- Change the products table category column from ENUM to text to match categories table
-ALTER TABLE products ALTER COLUMN category TYPE text;
